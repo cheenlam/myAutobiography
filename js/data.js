ae1eeb625fb774a6c8c2aae3541f3e98d1c04dd9
redsinsue <redsinsue@gmail.com>
2021-11-11 14:54:09 +0800

    no message

---
+++ js/data.js
@@ -0,0 +1,127 @@
+let skills = {
+    client: {
+        title: '前端開發',
+        list: ['HTML5', 'CSS', 'SCSS', 'Bootstrap', 'JavaScript', 'JQuery', 'javascriptES6', 'Vue', 'Git']
+    },
+    server: {
+        title: '後端開發',
+        list: ['MySQL', 'Java', "Dart"]
+    },
+    other: {
+        title: '其他',
+        list: ['Visual Studio Code', 'Sublime', 'Adobe Photoshop', 'Adobe Animate', 'AutoCAD', 'PLC']
+    }
+}
+
+let works = [{
+        href:'https://cheenlam.github.io/awinGuided/',
+        img: 'image/awinGuided.jpg',
+        info: [
+            '形象網頁',
+            '手刻RWD響應式網頁,排版使用SCSS撰寫',
+            '使用AOS外掛產生視差滾動效果'
+        ]
+    },
+    {
+        href:'https://cheenlam.github.io/vnc88/',
+        img: 'image/vnc88.jpg',
+        info: [
+            '推廣資訊網站',
+            '手刻RWD響應式網頁,排版使用SCSS撰寫',
+            '前台功能使用Vue架構撰寫',
+            '球賽/彩票/比分等相關資訊呈現,會員系統相關功能'
+        ]
+    },
+    {
+        href:'https://cheenlam.github.io/womenStore',
+        img: 'image/works_b01.jpg',
+        info: [
+            '女裝購物網',
+            '純手刻RWD響應式網頁。',
+            '使用JavaScript製作購物車功能，可將商品加入購物清單、刪除清單商品、顯示清單總數、總金額等功能。'
+        ]
+    },
+    {
+        href:'https://cheenlam.github.io/gamePanda',
+        img: 'image/works_b05.jpg',
+        info: [
+            '跑酷熊貓',
+            'HTML5 相關遊戲。',
+            '使用Adobe Animate在HTML中建立canvas元素，並以 Node.js 以及 JavaScript 語法控制相關物件演示畫面。',
+            '上方頭像區會根據畫面，進行名次變更，以便於呈現最後賽果。'
+        ]
+    },
+    {
+        href:'https://cheenlam.github.io/gameMonkey',
+        img: 'image/works_b07.jpg',
+        info: ['BB猴子爬樹',
+            'HTML5 相關遊戲。',
+            '參考BB猴子爬樹html遊戲練習製作，使用Adobe Animate在HTML中建立canvas元素，並以 Node.js 以及 JavaScript 語法控制相關物件演示畫面。',
+            '目前僅為靜態網頁，可使用網頁上按鈕做操控'
+        ]
+    },
+    {
+        href:'https://cheenlam.github.io/BB_HiLo',
+        img: 'image/works_b06.jpg',
+        info: [
+            'BB高低輪盤',
+            'HTML5 相關遊戲。',
+            '參考BB高低輪盤html遊戲練習製作，利用 純CSS 以及 JQuery 呈現各種動畫效果。',
+            '目前僅為靜態網頁，可使用網頁上按鈕做操控'
+        ]
+    },
+    {
+        href:'https://cheenlam.github.io/hilo_poker',
+        img: 'image/works_b03.jpg',
+        info: [
+            'HiLo Poker',
+            'HTML5 相關遊戲。',
+            '撲克牌猜高低遊戲，利用 純CSS 以及 JQuery 呈現各種動畫效果。',
+            '目前僅為靜態網頁，可使用網頁上按鈕做操控'
+        ]
+    },
+    {
+        href:'https://cheenlam.github.io/gamelFarm',
+        img: 'image/works_b04.jpg',
+        info: [
+            'RealFarm', '純手刻RWD響應式網頁。', '線上編輯該遊戲銷售清單網頁，使用localStorage儲存使用者相關資料。並崁入文字編輯器製作訊息輸入欄位。'
+        ]
+    },
+    {
+        href:'https://cheenlam.github.io/casino',
+        img: 'image/works_01.jpg',
+
+        info: [
+            '線上博弈入口網站',
+            '於資策會獨立開發的作品。', '手刻RWD響應式網頁，利用 transform屬性將圖片做出各種動畫效果。',
+            '使用JavaScript語法製作捲軸輪播。'
+        ]
+    },
+    {
+        href:'https://cheenlam.github.io/Roulette_Casino',
+        img: 'image/works_02.jpg',
+        info: [
+            '輪盤Roulette Casino(遊戲)',
+            '於資策會獨立開發的作品，網頁版輪盤遊戲。',
+            '參考真實賭場輪盤遊戲，以JavaScript監聽drop事件，判斷各別區塊籌碼疊加畫面，控制轉盤轉動，random亂數產生珠子落入的格子號碼，並判斷各下注區域賠率以及返回金額。'
+        ]
+    },
+    {
+        href:'https://cheenlam.github.io/sport_football',
+        img: 'image/works_03.jpg',
+        info: [
+            '世界杯球隊介紹網站',
+            '純手刻RWD響應式網頁。',
+            '以css與JavaScript搭配展現開頭LOGO及選單滑入動畫、倒數計時、購物清單輪播、選單動畫、捲軸滑動。'
+        ]
+    },
+    {
+        href:'https://cheenlam.github.io/acetic',
+        img: 'image/works_04.jpg',
+        info: [
+            '購物車網站',
+            '純手刻RWD響應式網頁。',
+            '使用JavaScript製作購物車功能，可將商品加入購物清單、刪除清單商品、顯示清單總數、總金額等功能。'
+        ]
+    },
+]
